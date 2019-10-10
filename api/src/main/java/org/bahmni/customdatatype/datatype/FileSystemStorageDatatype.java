f2bcd6a0a05ca505cd6fce4f91db121a4cb6640e
chinzou <chine.zoheir@gmail.com>
2019-10-10 12:10:14 +0800

    BAH-921: Un-staging formated file

--- api/src/main/java/org/bahmni/customdatatype/datatype/FileSystemStorageDatatype.java
+++ api/src/main/java/org/bahmni/customdatatype/datatype/FileSystemStorageDatatype.java
@@ -9,38 +9,34 @@ import java.io.UncheckedIOException;
 
 public class FileSystemStorageDatatype extends SerializingCustomDatatype<String> {
 
-	public static final String DEFAULT_ENCODING = "utf-8";
+    public static final String DEFAULT_ENCODING = "utf-8";
+    private String fileName;
 
-	private String fileName;
+    @Override
+    public void setConfiguration(String config) {
+        this.fileName = config;
+    }
 
-	@Override
-	public void setConfiguration(String config) {
-		this.fileName = config;
-	}
+    @Override
+    public String serialize(String typedValue) {
+        writeStringToFile(typedValue, new File(this.fileName));
+        return this.fileName;
+    }
 
-	@Override
-	public String serialize(String typedValue) {
-		writeStringToFile(typedValue, new File(this.fileName));
-		return this.fileName;
-	}
+    private void writeStringToFile(String typedValue, File file) {
+        try {
+            FileUtils.writeStringToFile(file, typedValue, DEFAULT_ENCODING);
+        } catch (IOException e) {
+            throw new UncheckedIOException("Unable to save the file with name ["+this.fileName+"]",e);
+        }
+    }
 
-	private void writeStringToFile(String typedValue, File file) {
-		try {
-			FileUtils.writeStringToFile(file, typedValue, DEFAULT_ENCODING);
-		}
-		catch (IOException e) {
-			throw new UncheckedIOException("Unable to save the file with name [" + this.fileName + "]", e);
-		}
-	}
-
-	@Override
-	public String deserialize(String fileName) {
-		try {
-			return FileUtils.readFileToString(new File(fileName), DEFAULT_ENCODING);
-		}
-		catch (IOException e) {
-			throw new UncheckedIOException(
-					"Unable to read from the file with name [" + new File(fileName).getAbsoluteFile() + "]", e);
-		}
-	}
+    @Override
+    public String deserialize(String fileName) {
+        try {
+            return FileUtils.readFileToString(new File(fileName), DEFAULT_ENCODING);
+        } catch (IOException e) {
+            throw new UncheckedIOException("Unable to read from the file with name [" + new File(fileName).getAbsoluteFile() + "]", e);
+        }
+    }
 }
