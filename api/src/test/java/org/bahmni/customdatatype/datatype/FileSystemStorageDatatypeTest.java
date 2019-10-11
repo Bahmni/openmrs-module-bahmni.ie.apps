473d36e227c5d7f2e16bba27008f131975f18298
chinzou <chine.zoheir@gmail.com>
2019-10-11 14:55:41 +0800

    BAH-921: Un-staging formated test file

--- api/src/test/java/org/bahmni/customdatatype/datatype/FileSystemStorageDatatypeTest.java
+++ api/src/test/java/org/bahmni/customdatatype/datatype/FileSystemStorageDatatypeTest.java
@@ -13,55 +13,55 @@ import static org.junit.Assert.assertEquals;
 
 public class FileSystemStorageDatatypeTest {
 
-	FileSystemStorageDatatype fileSystemStorageDatatype;
+    FileSystemStorageDatatype fileSystemStorageDatatype;
 
-	@Before
-	public void setUp() throws Exception {
-		fileSystemStorageDatatype = new FileSystemStorageDatatype();
-	}
+    @Before
+    public void setUp() throws Exception {
+        fileSystemStorageDatatype = new FileSystemStorageDatatype();
+    }
 
-	@Test
-	public void ensureThatTheValueIsStoredInFileSystemAndReferenceCaptured() throws Exception {
-		fileSystemStorageDatatype.setConfiguration("./testData_1.txt");
-		String fileName = fileSystemStorageDatatype.serialize("this is some sample data");
+    @Test
+    public void ensureThatTheValueIsStoredInFileSystemAndReferenceCaptured() throws Exception {
+        fileSystemStorageDatatype.setConfiguration("./testData_1.txt");
+        String fileName = fileSystemStorageDatatype.serialize("this is some sample data");
 
-		assertEquals(true, new File(fileName).exists());
-		assertEquals("./testData_1.txt", fileName);
-	}
+        assertEquals(true, new File(fileName).exists());
+        assertEquals("./testData_1.txt",fileName);
+    }
 
-	@Test
-	public void ensureThatTheValueIsOverwrittenInFileSystemAndReferenceCaptured() throws Exception {
-		File sampleDataFile = new File("./testData_2.txt");
-		FileUtils.writeStringToFile(sampleDataFile, "initial value");
+    @Test
+    public void ensureThatTheValueIsOverwrittenInFileSystemAndReferenceCaptured() throws Exception {
+        File sampleDataFile = new File("./testData_2.txt");
+        FileUtils.writeStringToFile(sampleDataFile,"initial value");
 
-		fileSystemStorageDatatype.setConfiguration("./testData_2.txt");
-		String fileName = fileSystemStorageDatatype.serialize("updated value");
+        fileSystemStorageDatatype.setConfiguration("./testData_2.txt");
+        String fileName = fileSystemStorageDatatype.serialize("updated value");
 
-		assertEquals(true, sampleDataFile.exists());
-		assertEquals("./testData_2.txt", fileName);
+        assertEquals(true, sampleDataFile.exists());
+        assertEquals("./testData_2.txt",fileName);
 
-		String actualValue = FileUtils.readFileToString(sampleDataFile);
-		assertEquals("updated value", actualValue);
-	}
+        String actualValue = FileUtils.readFileToString(sampleDataFile);
+        assertEquals("updated value", actualValue);
+    }
 
-	@Test
-	public void ensureThatTheFileContentIsReturned() throws IOException {
-		String fileName = "./testData_3.txt";
-		File sampleDataFile = new File(fileName);
-		FileUtils.writeStringToFile(sampleDataFile, "initial value");
+    @Test
+    public void ensureThatTheFileContentIsReturned() throws IOException {
+        String fileName = "./testData_3.txt";
+        File sampleDataFile = new File(fileName);
+        FileUtils.writeStringToFile(sampleDataFile,"initial value");
 
-		fileSystemStorageDatatype.setConfiguration(fileName);
-		String content = fileSystemStorageDatatype.deserialize(fileName);
+        fileSystemStorageDatatype.setConfiguration(fileName);
+        String content = fileSystemStorageDatatype.deserialize(fileName);
 
-		assertEquals("initial value", content);
-	}
+        assertEquals("initial value",content);
+    }
 
-	@Test(expected = UncheckedIOException.class)
-	public void ensureThatAProperExceptionIsReturnedWhenFileNameIsInvalid() {
-		String fileName = "something.txt";
+    @Test(expected = UncheckedIOException.class)
+    public void ensureThatAProperExceptionIsReturnedWhenFileNameIsInvalid(){
+        String fileName = "something.txt";
 
-		fileSystemStorageDatatype.setConfiguration(fileName);
-		fileSystemStorageDatatype.deserialize(fileName);
-	}
+        fileSystemStorageDatatype.setConfiguration(fileName);
+        fileSystemStorageDatatype.deserialize(fileName);
+    }
 
 }
